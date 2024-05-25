8ffc4bfecdb332fd0f3c2a6c17dc7e874d7a4fbc
salaheddine0899 <salaheddineabid99@gmail.com>
2024-05-25 14:00:41 +0100

    add exercise page

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ import 'package:mygym/src/bloc/workout.bloc.dart';
 import 'package:mygym/src/bloc/event/workout.event.dart';
 import 'package:mygym/src/data/repositories/database.dart';
 import 'package:mygym/src/presentation/pages/workout.page.dart';
+import 'package:mygym/src/presentation/pages/add.exercise.page.dart';
 import 'package:mygym/src/presentation/pages/home.page.dart';
 import 'package:mygym/src/presentation/pages/page1.dart';
 import 'package:mygym/src/presentation/pages/page2.dart';
 import 'package:mygym/src/presentation/pages/welcome.page.dart';
 import 'package:provider/provider.dart';
-
 
 void main() async {
   final appDatabase = AppDatabase();
@@ -28,7 +28,6 @@ void main() async {
   );
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -36,21 +35,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Sidebar Menu',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: GoogleFonts.robotoCondensedTextTheme(
-            Theme.of(context).textTheme,
-          ),
+      title: 'Sidebar Menu',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.robotoCondensedTextTheme(
+          Theme.of(context).textTheme,
         ),
-        home: const WelcomePage(),
-        routes: {
-          '/home': (context) => MyHomePage(),
-          '/page1': (context) => Page1(),
-          '/page2': (context) => Page2(),
-          '/workout': (context) => WorkoutPage(),
-          // Add more routes as needed
-        },
-      );
+      ),
+      home: const WelcomePage(),
+      routes: {
+        '/home': (context) => MyHomePage(),
+        '/page1': (context) => Page1(),
+        '/page2': (context) => Page2(),
+        '/workout': (context) => WorkoutPage(),
+        '/add-exercise': (context) => AddExercisePage(),
+        // Add more routes as needed
+      },
+    );
   }
 }
