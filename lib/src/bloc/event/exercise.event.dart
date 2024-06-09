2a8fef9795a4bea515a58a23930c9d670820c7c1
salaheddine0899 <salaheddineabid99@gmail.com>
2024-06-09 21:41:45 +0100

    add and exercise list

--- lib/src/bloc/event/exercise.event.dart
+++ lib/src/bloc/event/exercise.event.dart
@@ -5,10 +5,7 @@ class LoadExercises extends ExerciseEvent {}
 class AddExercise extends ExerciseEvent {
   final int workoutId;
   final int exerciseItemId;
-  final int repetitions;
-  final int points;
-  AddExercise(
-      this.workoutId, this.exerciseItemId, this.repetitions, this.points);
+  AddExercise(this.workoutId, this.exerciseItemId);
 }
 
 class LoadExerciseByWorkoutId extends ExerciseEvent {
